9e67acbb81a306bbb773b800d82e8609cd743635
Sun choi <csun2772@naver.com>
2024-08-13 15:49:30 +0900

    git init

---
+++ src/main/java/com/sparta/schedulertask/SchedulerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.schedulertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerTaskApplication.class, args);
+    }
+
+}
